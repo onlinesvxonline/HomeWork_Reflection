576cc864e4e11bffd69b54e6a684a2e0a70b1a8f
Denis Chornonog <onlinesvxonline@mail.ru>
2024-05-03 13:16:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class CustomNameAttribute : Attribute
+{
+    public string Name { get; }
+
+    public CustomNameAttribute(string name)
+    {
+        Name = name;
+    }
+}
+
+public class CustomObject
+{
+    [CustomName("CustomFieldName")]
+    public int I = 7;
+
+    public string ObjectToString()
+    {
+        string result = "";
+
+        foreach (var field in GetType().GetFields())
+        {
+            CustomNameAttribute attribute = (CustomNameAttribute)Attribute.GetCustomAttribute(field, typeof(CustomNameAttribute));
+            string propertyName = attribute != null ? attribute.Name : field.Name;
+
+            result += $"{propertyName}:{field.GetValue(this)} ";
+        }
+
+        return result.Trim();
+    }
+
+    public void StringToObject(string data)
+    {
+        string[] pairs = data.Split(' ');
+
+        foreach (string pair in pairs)
+        {
+            string[] parts = pair.Split(':');
+            string propertyName = parts[0];
+            string propertyValue = parts[1];
+
+            foreach (var field in GetType().GetFields())
+            {
+                CustomNameAttribute attribute = (CustomNameAttribute)Attribute.GetCustomAttribute(field, typeof(CustomNameAttribute));
+                if (attribute != null && attribute.Name == propertyName)
+                {
+                    field.SetValue(this, Convert.ChangeType(propertyValue, field.FieldType));
+                }
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CustomObject obj = new CustomObject();
+        string data = obj.ObjectToString();
+        Console.WriteLine(data);
+
+        CustomObject newObj = new CustomObject();
+        newObj.StringToObject(data);
+
+        // Проверка, что значения были скопированы корректно
+        Console.WriteLine($"Проверка: {newObj.I}");
+    }
+}
